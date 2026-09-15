5fa835f3108a195d3d1c02b260404a8fd45a6636
nsushma-bd <nsushma@blackduck.com>
2026-09-15 13:18:57 +0530

    update file test/api/basketItemApiSpec.ts

--- test/api/basketItemApiSpec.ts
+++ test/api/basketItemApiSpec.ts
@@ -18,7 +18,7 @@ beforeAll(() => {
     headers: jsonHeader,
     body: {
       email: 'jim@' + config.get<string>('application.domain'),
-      password: 'ncc-1701'
+      password: config.get<string>('test.user.password')
     }
   })
     .expect('status', 200)
